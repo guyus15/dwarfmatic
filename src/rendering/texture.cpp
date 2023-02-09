7a715a5bf5755f8dff9e87627cb7f6a630fbefa5
Guy Chamberlain-Webber <chamberlainwebberj@gmail.com>
2023-02-09 00:07:29 +0000

    Implemented class for texture abstraction

---
+++ src/rendering/texture.cpp
@@ -0,0 +1,55 @@
+#include "texture.h"
+
+#include <iostream>
+
+Texture2D::Texture2D()
+    : m_id{},
+    m_internal_format{ GL_RGB },
+    m_image_format{ GL_RGB },
+    m_wrap_s{ GL_REPEAT },
+    m_wrap_t{ GL_REPEAT },
+    m_filter_min{ GL_LINEAR },
+    m_filter_mag{ GL_LINEAR },
+    m_width{},
+    m_height{}
+{
+}
+
+Texture2D::~Texture2D()
+{
+    glDeleteTextures(1, &m_id);
+}
+
+void Texture2D::Generate(const int width, const int height, unsigned char* data)
+{
+    glGenTextures(1, &m_id);
+    glBindTexture(GL_TEXTURE_2D, m_id);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, m_wrap_s);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, m_wrap_t);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, m_filter_min);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, m_filter_mag);
+
+    m_width = width;
+    m_height = height;
+
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, m_internal_format, m_width, m_height, 0, m_image_format, GL_UNSIGNED_BYTE, data);
+    }
+    else
+    {
+        std::cerr << "Error: Failed to generate texture.\n";
+        return;
+    }
+
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    // Unbind texture
+    glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+void Texture2D::Bind() const
+{
+    glBindTexture(GL_TEXTURE_2D, m_id);
+}
---
+++ src/rendering/texture.h
@@ -0,0 +1,30 @@
+#ifndef TEXTURE_H
+#define TEXTURE_H
+
+#include "glad/glad.h"
+
+class Texture2D
+{
+public:
+    Texture2D();
+    ~Texture2D();
+
+    Texture2D(const Texture2D&) = default;
+    Texture2D(Texture2D&&) noexcept = default;
+
+    Texture2D& operator=(const Texture2D&) = default;
+    Texture2D& operator=(Texture2D&&) noexcept = default;
+
+    void Generate(int width, int height, unsigned char* data);
+    void Bind() const;
+
+private:
+    GLuint m_id;
+    GLint m_internal_format;
+    GLint m_image_format;
+    GLint m_wrap_s, m_wrap_t;
+    GLint m_filter_min, m_filter_mag;
+    GLsizei m_width, m_height;
+};
+
+#endif // TEXTURE_H
